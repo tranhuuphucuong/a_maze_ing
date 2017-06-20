454b35359ab7ccd6527c3b37cc07aa210cca0779
gt-cuongtran <cuong.tran.asiantech@gmail.com>
2017-06-20 15:43:41 +0700

    hot fix

--- lib/a_maze_ing/cli.rb
+++ lib/a_maze_ing/cli.rb
@@ -8,9 +8,11 @@ module AMazeIng
     def run
       @full_screen = false
 
-      program :name, AMazeIng::Configuration::PROGRAM_NAME
+      # program :name, AMazeIng::Configuration::PROGRAM_NAME 
+      program :name "AMazeIng"
       program :version, AMazeIng::VERSION
-      program :description, AMazeIng::Configuration::DESCRIPTION
+      # program :description, AMazeIng::Configuration::DESCRIPTION 
+      program :description, "Maze solving game... don't be addicted" 
 
       default_command :classic
 
